0eed0f4032204a59d06d712c35374c1269ea7a89
Bjorn <bjossijohnson@gmail.com>
2017-12-06 14:07:30 +0100

    gitIgnore

---
+++ code/src/components/cart/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+class Cart extends React.Component { mmmmmmm
+  render() {
+    return (
+      <div className="cartContainer">
+      </div>
+    )
+  }
+}
+
+export default Cart
