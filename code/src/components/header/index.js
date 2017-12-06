6d79bcdaa901773553cba15f2ee45047f1fa12ed
Emelie Sidesiö <esidesio@gmail.com>
2017-12-06 16:15:15 +0100

    header added

--- code/src/components/header/index.js
+++ code/src/components/header/index.js
@@ -5,14 +5,12 @@ class Header extends React.Component {
   render() {
     return(
       <div className="header-container">
+        <div className="symbols-container">
+          <div>Cart</div>
+          <div>Search</div>
+        </div>
         <div className="logo-container">
           <h1>Watches & Art</h1>
-          <div className="symbols-container">
-            <ul>
-              <li>Cart</li>
-              <li>Search</li>
-            </ul>
-          </div>
         </div>
       </div>
     )
