620c7ce6b6f514fa257a704750f21ecc1d1b8d9d
Cecilia Wong <cissi.wong@gmail.com>
2017-12-06 10:51:53 +0100

    added allProducts component

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -4,13 +4,30 @@ import Header from "./header"
 import Products from "./products"
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      allProducts: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://api.tictail.com/v1.26/stores/5zns/products").then((response) => {
+      return response.json()
+    }).then((json) => {
+      this.setState({ allProducts: json })
+    })}
 
   render() {
     return (
-      <div>
-        <h1>Watches & Art</h1>
-        <h2>100 sek</h2>
-        <p>Product description bla bla bla</p>
+      <div className="allProducts" >
+        {this.state.allProducts.map((item) => {
+          return <div
+            key={item.id}
+            title={item.title}
+            price={item.price}
+           image={item.images.url} />
+        })}
       </div>
     )
   }
--- code/src/components/products/index.js
+++ code/src/components/products/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 class Products extends React.Component {
+  
 
 }
 
