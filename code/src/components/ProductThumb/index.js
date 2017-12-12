ac5d87ebf48dfa834c9a891c2d53dab6d50313c1
Katarina Sjölund <katarina.sjolund@outlook.com>
2017-12-12 14:07:35 +0100

    update link

--- code/src/components/ProductThumb/index.js
+++ code/src/components/ProductThumb/index.js
@@ -5,16 +5,16 @@ import "./index.css"
 class ProductThumb extends React.Component {
   render() {
     return (
-      <Link to={`/product/${this.props.id}`}>
-        <div className="ProductThumb">
+      <div className="ProductThumb">
+        <Link to={`/product/${this.props.id}`}>
           <img className="product-image" src={this.props.image} alt="" />
-          <div className="product-info">
-            <div className="product-title">{this.props.title}</div>
-            <div className="product-price">{this.props.price} {this.props.currency}</div>
-            <button className="product-btn">Buy</button>
-          </div>
+        </Link>
+        <div className="product-info">
+          <div className="product-title">{this.props.title}</div>
+          <div className="product-price">{this.props.price} {this.props.currency}</div>
+          <button className="product-btn">Buy</button>
         </div>
-      </Link>
+      </div>
     )
   }
 }
--- code/src/pages/CategoriesPage/index.js
+++ code/src/pages/CategoriesPage/index.js
@@ -9,6 +9,24 @@ class CategoriesPage extends React.Component {
     }
   }
 
+  // componentDidMount() {
+  //   fetch(`https://api.tictail.com/v1.26/stores/5zns/categories${this.props.id}`).then(response => {
+  //     console.log(response)
+  //     return response.json()
+  //   }).then(json => {
+  //     this.setState({ categories: json })
+  //   })
+  // }
+  //
+  // render() {
+  //   return (
+  //     // id={this.state.match.params.categoriesid}
+  //     <div className="category-container">
+  //       {this.state.categories.product_count}
+  //     </div>
+  //   )
+  // }
+
   componentDidMount() {
     fetch("https://api.tictail.com/v1.26/stores/5zns/categories").then(response => {
       return response.json()
