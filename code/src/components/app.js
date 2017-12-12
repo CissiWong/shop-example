3cfa90ade41f7a1ac75753eacab050004432b18d
Bjorn <bjossijohnson@gmail.com>
2017-12-12 10:25:32 +0100

    homepage component removed from app

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -13,7 +13,6 @@ class App extends React.Component {
       <BrowserRouter>
         <div className="App">
           <Header />
-          {/* <Homepage /> */}
           <div className="page">
             <Route path="/" exact component={Homepage} />
             <Route path="/categories" component={Categories} />
