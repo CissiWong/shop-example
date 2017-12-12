409ed4e446958476aaf83a1ecff6fa2b6ac5c433
Emelie Sidesiö <esidesio@gmail.com>
2017-12-12 18:41:26 +0100

    home page fixes

--- code/src/components/ProductThumb/index.js
+++ code/src/components/ProductThumb/index.js
@@ -5,14 +5,16 @@ import "./index.css"
 class ProductThumb extends React.Component {
   render() {
     return (
-      <div className="ProductThumb">
-        <Link to={`/product/${this.props.id}`}>
-          <img className="product-image" src={this.props.image} alt="" />
-        </Link>
-        <div className="product-info">
-          <div className="product-title">{this.props.title}</div>
-          <div className="product-price">{(this.props.price / 100).toLocaleString()} {this.props.currency}</div>
-          <button className="product-btn">Add to Cart</button>
+      <div className="ProductThumb-container">
+        <div className="ProductThumb">
+          <Link to={`/product/${this.props.id}`}>
+            <img className="product-image" src={this.props.image} alt="" />
+          </Link>
+          <div className="product-info">
+            <div className="product-title">{this.props.title}</div>
+            <div className="product-price">{(this.props.price / 100).toLocaleString()} {this.props.currency}</div>
+            <button className="product-btn">Add to Cart</button>
+          </div>
         </div>
       </div>
     )
