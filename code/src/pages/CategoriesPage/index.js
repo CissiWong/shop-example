1996a893494a123d066cb2009a8b992e725bb1cf
Bjorn <bjossijohnson@gmail.com>
2017-12-13 11:01:34 +0100

    categories id

--- code/src/pages/CategoriesPage/index.js
+++ code/src/pages/CategoriesPage/index.js
@@ -1,5 +1,7 @@
 import React from "react"
+import ProductThumb from "components/ProductThumb"
 import "./index.css"
+
 
 class CategoriesPage extends React.Component {
   constructor(props) {
@@ -9,14 +11,17 @@ class CategoriesPage extends React.Component {
     }
   }
 
-  // componentDidMount() {
-  //   fetch(`https://api.tictail.com/v1.26/stores/5zns/categories${this.props.id}`).then(response => {
-  //     console.log(response)
-  //     return response.json()
-  //   }).then(json => {
-  //     this.setState({ categories: json })
-  //   })
-  // }
+  componentDidMount() {
+    const catId = this.props.match.params.categoriesid
+    fetch(`https://api.tictail.com/v1.26/stores/5zns/products?categories=${catId}`).then(response => {
+      console.log(response)
+      return response.json()
+    }).then(json => {
+      console.log(json)
+      this.setState({ categories: json })
+    })
+  }
+  // `https://api.tictail.com/v1.26/stores/5zns/products?categories=${this.props.id}`
   //
   // render() {
   //   return (
@@ -27,21 +32,31 @@ class CategoriesPage extends React.Component {
   //   )
   // }
 
-  componentDidMount() {
-    fetch("https://api.tictail.com/v1.26/stores/5zns/categories").then(response => {
-      return response.json()
-    }).then(json => {
-      this.setState({ categories: json })
-    })
-  }
+  // https://api.tictail.com/v1.25/stores/5zns/products?categories=a3mh
+
+  // componentDidMount() {
+  //   fetch("https://api.tictail.com/v1.26/stores/5zns/categories").then(response => {
+  //     return response.json()
+  //   }).then(json => {
+  //     this.setState({ categories: json })
+  //   })
+  // }
 
   render() {
     return (
       <div className="Categories">
         {this.state.categories.map(item => {
-          return <div className="categories-container">
-            {item.title}
-          </div>
+          return <ProductThumb
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            price={item.price}
+            image={item.images[0].url}
+            description={item.description}
+            currency={item.currency}
+            categories={item.categories[0].slug}
+            onSale={item.sale_active}
+            originalPrice={item.original_price} />
         })}
       </div>
     )
