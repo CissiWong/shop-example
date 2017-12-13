369321b8454937407b82cceb3356c8e6c1dd937a
Cecilia Wong <cissi.wong@gmail.com>
2017-12-13 13:32:46 +0100

    added category count

--- code/src/pages/CategoriesPage/index.js
+++ code/src/pages/CategoriesPage/index.js
@@ -33,7 +33,7 @@ class CategoriesPage extends React.Component {
   render() {
     return (
       <div className="CategoryCollection">
-        
+        {this.state.categories.length}
         {this.state.categories.map(item => {
           return <ProductThumb
             key={item.id}
