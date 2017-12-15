853285914ceb71147b2a8e51e3244140fcc5a011
Emelie Sidesiö <esidesio@gmail.com>
2017-12-15 08:15:15 +0100

    code clean up

--- code/src/components/footer/index.js
+++ code/src/components/footer/index.js
@@ -8,7 +8,6 @@ class Footer extends React.Component {
       <div className="footer">
         <a className="termsOfService" href="/terms-of-service"><span>Terms of Service</span></a>
         <a className="privacyPolicy" href="/privacy-policy">Privacy Policy</a>
-        {/* <a className="helpCenter" href="//help.tictail.com">Help Center</a> */}
         <button className="helpCenter">Helpcenter</button>
       </div>
     )
